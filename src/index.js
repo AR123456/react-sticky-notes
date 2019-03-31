6ac69102d8fae35de08c30bde6ab66cd39ba95aa
AR123456 <coloradoz99@gmail.com>
2019-03-31 06:05:35 -0600

    refactor with lifecycle methods

--- src/index.js
+++ src/index.js
@@ -2,25 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-  //special javascript function called constructor, it will be run before anything else - it can be used to initialize state
-  constructor(props) {
-    //super is a ref to the parent function and is just neccesary
-    super(props);
-    //this is the only time we do a direct assingment of state, ( inside the constructor function)
-    this.state = { lat: null, errorMessage: "" };
-
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+  //moving set state out of the constructor
+  state = { lat: null, errorMessage: "" };
+  // moved this code block out of the constructor above
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        //we called setstate
-        this.setState({ lat: position.coords.latitude });
-      },
-      //make this error more helpful by adding state so we can re render
-      err => {
-        this.setState({ errorMessage: err.message });
-      }
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
-
   //React says we have to define render
   render() {
     if (this.state.errorMessage && !this.state.lat) {
