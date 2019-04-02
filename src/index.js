b0c562b3e321890e29a41f0875cd1202f1e9aeb7
AR123456 <coloradoz99@gmail.com>
2019-04-02 05:23:56 -0600

    cars initial set up

--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from "./SeasonDisplay";
+import App from "./components/App";
 
-class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { lat: null, errorMessage: "" };
-  // }
-  //moving set state out of the constructor
-  state = { lat: null, errorMessage: "" };
-  // moved this code block out of the constructor above
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
-  }
-  //React says we have to define render
-  render() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error:{this.state.errorMessage}</div>;
-    }
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-    return <div>Loading </div>;
-  }
-}
 ReactDOM.render(<App />, document.querySelector("#root"));
