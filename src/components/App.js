9ce74d2fcf62faeafefa8c031d633d58c5ef5f27
AR123456 <coloradoz99@gmail.com>
2019-04-14 05:59:22 -0600

    handle request await async

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,19 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  // using async and await
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
-        Authorization: removed
+        Authorization: "Client-ID "
       }
     });
+    //can use a .then when waiting for promise
+    // .then(response => {
+    //   console.log(response.data.results);
+    // });
+    console.log(response.data.results);
   }
   render() {
     return (
