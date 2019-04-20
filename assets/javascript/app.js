4aa11b7d6f119f7cde2ea12b432a9b255afb34e2
AR123456 <coloradoz99@gmail.com>
2019-04-20 06:20:47 -0600

    forEach call back

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,30 @@
-var timerId = setTimeout(function() {
-  console.log("This function runs in 30 seconds");
-}, 30000);
-setTimeout(function() {
-  console.log("Canceling the first setTimeout", timerId);
-  clearTimeout(timerId);
-}, 2000);
+// **************** Old way useing a for loop
 
-var num = 0;
-var intervalId = setInterval(function() {
-  num++;
-  console.log("num:", num);
-  if (num === 3) {
-    clearInterval(intervalId);
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function double(arr) {
+//   for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i] * 2);
+//   }
+// }
+// double(arr);
+
+// //////////// using forEach//////////////////
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+arr.forEach(function(number) {
+  console.log(number * 2);
+});
+
+//  This forEach uses all three parameters avalible in the forEach
+var strings = ["my", "forEach", "example"];
+
+var results = "";
+strings.forEach(function(str, index, array) {
+  if (array.length - 1 !== index) {
+    results += str + " ";
+  } else {
+    results += str + "!!!";
   }
-}, 1000);
+});
+console.log(results);
+//  the reuslt is " my forEach exapmple "
