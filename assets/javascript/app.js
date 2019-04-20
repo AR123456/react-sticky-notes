eeb2a91aeb81c1b97a5063173aa44168d926bc09
AR123456 <coloradoz99@gmail.com>
2019-04-20 06:19:25 -0600

    JS set Timout

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var timerId = setTimeout(function() {
+  console.log("This function runs in 30 seconds");
+}, 30000);
+setTimeout(function() {
+  console.log("Canceling the first setTimeout", timerId);
+  clearTimeout(timerId);
+}, 2000);
+
+var num = 0;
+var intervalId = setInterval(function() {
+  num++;
+  console.log("num:", num);
+  if (num === 3) {
+    clearInterval(intervalId);
+  }
+}, 1000);
