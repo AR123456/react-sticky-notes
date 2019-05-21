5888302a8198060c53751789df81723dab7ebca4
AR123456 <coloradoz99@gmail.com>
2019-05-21 04:41:27 -0600

    css grid

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -2,7 +2,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
-    Authorization:
-      "Client-ID "
+    Authorization: "Client-ID "
   }
 });
