70deedc0aef9a263b2af3e67fcb5eea4f9785dbb
AR123456 <coloradoz99@gmail.com>
2019-05-28 06:23:04 -0600

    React D3 Chart Back Box HOC

--- src/components/Axis.js
+++ src/components/Axis.js
@@ -1,32 +1,13 @@
-import React, { Component } from "react";
+import D3blackbox from "./D3blackbox";
 import * as d3 from "d3";
 
-class Axis extends Component {
-  gRef = React.createRef();
+const Axis = D3blackbox(function() {
+  const scale = d3
+    .scaleLinear()
+    .domain([0, 10])
+    .range([0, 200]);
+  const axis = d3.axisBottom(scale);
 
-  componentDidMount() {
-    this.d3render();
-  }
-  componentDidUpdate() {
-    this.d3render();
-  }
-
-  d3render() {
-    const scale = d3
-      .scaleLinear()
-      .domain([0, 10])
-      .range([0, 200]);
-
-    const axis = d3.axisBottom(scale);
-
-    d3.select(this.gRef.current).call(axis);
-  }
-
-  render() {
-    const { y } = this.props;
-
-    return <g transform={`translate(10, 10)`} ref={this.gRef} />;
-  }
-}
-
+  d3.select(this.anchor.current).call(axis);
+});
 export default Axis;
