74da70711983dc5c5bf975787a4eace25845efce
AR123456 <coloradoz99@gmail.com>
2019-12-12 06:29:00 -0700

    implementing react router

--- src/components/containers/App.js
+++ src/components/containers/App.js
@@ -1,6 +1,15 @@
 import React from "react";
 import Player from "./Player";
+import {BrowserRouter,Route,Switch} from "react-router-dom"
 
-const App = () => <Player></Player>;
+
+const App = () => (
+    <BrowserRouter>
+    <Switch>
+        <Route exact path="/" component ={Player}/> 
+        <Route exact path="/:activeVideo" component ={Player}/> 
+    </Switch>
+    </BrowserRouter>
+);
 
 export default App;
