de0c0fca9b08515f058bdbf57f0c215a49b6b4fc
AR123456 <coloradoz99@gmail.com>
2020-01-07 04:33:29 -0700

    check collision with wall

--- src/App.js
+++ src/App.js
@@ -19,25 +19,59 @@ const App = () => {
   const [gameOver, setGameover] = useState(false);
 
   const startGame = () => {
-    //
+    setSnake(SNAKE_START);
+    setApple(APPLE_START);
+    setDir([0, -1]);
+    setSpeed(SPEED);
+    setGameover(false);
   };
   const endGame = () => {
-    //
+    setSpeed(null);
+    setGameover(true);
   };
-  const moveSnake = () => {
-    //
-  };
+  const moveSnake = ({ keyCode }) =>
+    // only move when arrow keys are used
+    keyCode >= 37 && keyCode <= 40 && setDir(DIRECTIONS[keyCode]);
   const createApple = () => {
     //
   };
-  const checkCollision = () => {
-    //
+  const checkCollision = (piece, snk = snake) => {
+    // check for collision with walls
+    if (
+      // piece is head of snake
+      // x val * scale greater than or = canvas size width
+      piece[0] * SCALE >= CANVAS_SIZE[0] ||
+      // or x val less than 0
+      piece[0] < 0 ||
+      //y val *scale greater than or = canvas size height
+      piece[1] * SCALE >= CANVAS_SIZE[1] ||
+      // or y less than 0
+      piece[1] < 0
+    ) {
+      return true;
+    } else {
+      return false;
+    }
   };
+
   const checkAppleCollision = () => {
-    //
+    //check collision with apple make sure that when we first get random apple it is not inside the snake
   };
   const gameLoop = () => {
-    //
+    //move snake make copy of snake - deep clone since it is a multidimentional array
+    const snakeCopy = JSON.parse(JSON.stringify(snake));
+    // make new snake head
+    // calc new x coord looking at the copy's x posiiton and dir  x is the first values in their respective arrays y is the second value
+    const newSnakeHead = [snakeCopy[0][0] + dir[0], snakeCopy[0][1] + dir[1]];
+    // mutate the snakecopy and attach the new head to the start of the array
+    snakeCopy.unshift(newSnakeHead);
+    // check for collison
+    if (checkCollision(newSnakeHead)) endGame();
+    //remove the last element in the array - this is how we get snake to crawl
+
+    snakeCopy.pop();
+    // update state
+    setSnake(snakeCopy);
   };
   //  this use effect is what actualy draws to the canvas
   useEffect(() => {
@@ -57,6 +91,8 @@ const App = () => {
     //draw rect and pull apple out of state
     context.fillRect(apple[0], apple[1], 1, 1);
   }, [snake, apple, gameOver]);
+
+  useInterval(() => gameLoop(), speed);
 
   return (
     <div role="button" tabIndex="0" onKeyDown={e => moveSnake(e)}>
