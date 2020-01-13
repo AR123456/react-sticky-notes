bf4f859147b9308454d3597224685932aad93f34
AR123456 <coloradoz99@gmail.com>
2020-01-13 05:14:31 -0700

    D3 + React+Hooks+BlackBox method

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import * as d3 from "d3";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import "./styles.css";
+
+import recaman from "./recaman";
+import dashes from "./animated-dash";
+
+const D3blackbox = ({ x, y, render }) => {
+  const refAnchor = React.useRef(null);
+
+  React.useEffect(() => {
+    render(d3.select(refAnchor.current));
+  });
+
+  return <g ref={refAnchor} transform={`translate(${x}, ${y})`} />;
+};
+
+function App() {
+  return (
+    <div className="App">
+      <svg width="900" height="900">
+        <D3blackbox x={0} y={0} render={svg => recaman(svg, 400, 400)} />
+        <D3blackbox x={0} y={400} render={svg => dashes(svg, 300, 200)} />
+      </svg>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
